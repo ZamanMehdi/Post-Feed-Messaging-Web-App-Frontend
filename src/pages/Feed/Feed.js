52a518b6e2b3cd42311f785092359cd91da28768
Zaman Mehdi <zamanmehdi110@gmail.com>
2020-07-13 00:07:51 +0500

    delete post done

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -120,7 +120,6 @@ class Feed extends Component {
     let method = 'POST'
     if (this.state.editPost) {
       url = url + '/' + this.state.editPost._id;
-      // url = 'http://localhost:8080/feed/post/' + this.state.editPost._id;
       method = 'PUT';
     }
 
@@ -178,7 +177,9 @@ class Feed extends Component {
 
   deletePostHandler = postId => {
     this.setState({ postsLoading: true });
-    fetch('URL')
+    fetch('http://localhost:8080/feed/post/'+ postId, {
+      method: 'DELETE'
+    })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
           throw new Error('Deleting a post failed!');
